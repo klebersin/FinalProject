920d6af61fd8b8f20b7106be4ed40964341a7f5e
klebersin <kleber.sik@gmail.com>
2021-08-08 13:06:28 -0500

    'f'

---
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaRecorder;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MediaRecorder mediaRecorder;
+    private String filename=null;
+    private Button btn_start;
+    private Button btn_stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_start = (Button) findViewById(R.id.btn_start);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1000);
+        }
+
+    }
+    public void startRecord(View view){
+        if(mediaRecorder == null){
+            filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.mp3";
+            mediaRecorder = new MediaRecorder();
+            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            mediaRecorder.setOutputFile(filename);
+
+            try {
+                mediaRecorder.prepare();
+                mediaRecorder.start();
+            }catch (IOException e){
+
+            }
+            btn_start.setText("Detener");
+            Toast.makeText(getApplicationContext(), "Grabando", Toast.LENGTH_SHORT).show();
+
+        }
+        else if(mediaRecorder!=null) {
+            mediaRecorder.stop();
+            mediaRecorder.release();
+            mediaRecorder = null;
+            btn_start.setText("Iniciar");
+            Toast.makeText(getApplicationContext(), "Parando grabación", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
