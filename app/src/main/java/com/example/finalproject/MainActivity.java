c6802a236a03890e274b7b9984206012b5bf1636
klebersin <kleber.sik@gmail.com>
2021-08-08 19:09:36 -0500

    convert

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.finalproject;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.os.Environment;
@@ -9,61 +11,123 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import com.musicg.fingerprint.FingerprintManager;
-import com.musicg.fingerprint.FingerprintSimilarity;
-import com.musicg.fingerprint.FingerprintSimilarityComputer;
 import com.musicg.wave.Wave;
-import javazoom.jl.converter.Converter;
+
 import javazoom.jl.decoder.JavaLayerException;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MediaRecorder mediaRecorder;
+    private static final int SAMPLING_RATE_IN_HZ = 44100;
+    private static final int CHANNEL_CONFIG = AudioFormat.CHANNEL_IN_MONO;
+    private static final int AUDIO_FORMAT = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int BUFFER_SIZE_FACTOR = 2;
+
+    private static final int BUFFER_SIZE = AudioRecord.getMinBufferSize(SAMPLING_RATE_IN_HZ,
+            CHANNEL_CONFIG, AUDIO_FORMAT) * BUFFER_SIZE_FACTOR;
+    private final AtomicBoolean recordingInProgress = new AtomicBoolean(false);
     private String filename=null;
     private Button btn_start;
-    private Button btn_stop;
+    private AudioRecord recorder = null;
+
+    private Thread recordingThread = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_start = (Button) findViewById(R.id.btn_start);
-
         if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO}, 1000);
         }
 
     }
-    public void startRecord(View view) throws JavaLayerException{
-        if(mediaRecorder == null){
-            filename = Environment.getExternalStorageDirectory().getAbsolutePath() + "/record.mp3";
-            mediaRecorder = new MediaRecorder();
-            mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
-            mediaRecorder.setOutputFile(filename);
+    public void startRecord(View view) {
+        if(recorder == null){
+            recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, SAMPLING_RATE_IN_HZ,
+                    CHANNEL_CONFIG, AUDIO_FORMAT, BUFFER_SIZE);
 
-            try {
-                mediaRecorder.prepare();
-                mediaRecorder.start();
-            }catch (IOException e){
+            recorder.startRecording();
 
-            }
+            recordingInProgress.set(true);
+
+            recordingThread = new Thread(new RecordingRunnable(), "Recording Thread");
+            recordingThread.start();
             btn_start.setText("Detener");
             Toast.makeText(getApplicationContext(), "Grabando", Toast.LENGTH_SHORT).show();
 
         }
-        else if(mediaRecorder!=null) {
-            mediaRecorder.stop();
-            mediaRecorder.release();
-            mediaRecorder = null;
+        else if(recorder!=null) {
+            recordingInProgress.set(false);
+
+            recorder.stop();
+
+            recorder.release();
+
+            recorder = null;
+
+            recordingThread = null;
             btn_start.setText("Iniciar");
             Toast.makeText(getApplicationContext(), "Parando grabación", Toast.LENGTH_SHORT).show();
         }
 
+    }
+    public void compare(View view) throws JavaLayerException, IOException {
+        File file = new File(filename+".pcm");
+        File file2 = new File(filename+".wav");
+        Convert.PCMToWAV(file,file2,1,8000 ,2);
+        byte[] firstFingerPrint = new FingerprintManager().extractFingerprint(new Wave(filename));
+    }
+
+    private class RecordingRunnable implements Runnable {
+
+        @Override
+        public void run() {
+            filename = Environment.getExternalStorageDirectory()+"/record";
+            final File file = new File(filename+".pcm");
+            final ByteBuffer buffer = ByteBuffer.allocateDirect(BUFFER_SIZE);
+
+            try{
+
+                final FileOutputStream outStream = new FileOutputStream(file);
+                while (recordingInProgress.get()) {
+                    int result = recorder.read(buffer, BUFFER_SIZE);
+                    if (result < 0) {
+                        throw new RuntimeException("Reading of audio buffer failed: " +
+                                getBufferReadFailureReason(result));
+                    }
+                    outStream.write(buffer.array(), 0, BUFFER_SIZE);
+                    buffer.clear();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException("Writing of recorded audio failed", e);
+            }
+        }
+
+        private String getBufferReadFailureReason(int errorCode) {
+            switch (errorCode) {
+                case AudioRecord.ERROR_INVALID_OPERATION:
+                    return "ERROR_INVALID_OPERATION";
+                case AudioRecord.ERROR_BAD_VALUE:
+                    return "ERROR_BAD_VALUE";
+                case AudioRecord.ERROR_DEAD_OBJECT:
+                    return "ERROR_DEAD_OBJECT";
+                case AudioRecord.ERROR:
+                    return "ERROR";
+                default:
+                    return "Unknown (" + errorCode + ")";
+            }
+        }
     }
 }
